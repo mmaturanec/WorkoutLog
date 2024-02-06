338eb3b29f23c539c19aecd4fd69e941a1e086a3
mmaturanec <matej.maturanec@gmail.com>
2024-02-06 20:47:09 +0100

    fixanje kod prijava btn pressa ako nisu upisani svi podatci

--- app/src/main/java/com/example/workoutlog/MainActivity.java
+++ app/src/main/java/com/example/workoutlog/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -67,27 +68,33 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 sEmail = etLoginUserName.getText().toString();
                 sPassword = etLoginLozinka.getText().toString();
+                if(TextUtils.isEmpty(sEmail) || TextUtils.isEmpty(sPassword))
+                {
+                    Toast.makeText(MainActivity.this, getString(R.string.UnesiPasswordemial), Toast.LENGTH_SHORT).show();
+                }
+                else {
                 /*
                 Intent intentMainMenu = new Intent(MainActivity.this, MainMenuActivity.class);
                 startActivity(intentMainMenu);
 
                  */
-                mAuth.signInWithEmailAndPassword(sEmail, sPassword)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(MainActivity.this, getResources().getString(R.string.PogresnoUneseniPodatci),
-                                            Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
+                    mAuth.signInWithEmailAndPassword(sEmail, sPassword)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(MainActivity.this, getResources().getString(R.string.PogresnoUneseniPodatci),
+                                                Toast.LENGTH_SHORT).show();
+                                        updateUI(null);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
 
             }
         });
